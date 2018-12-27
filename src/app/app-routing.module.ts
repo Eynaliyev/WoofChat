863d588f4be0bfb6134ea87daf9480b1628ab36f
Rustam Eynaliyev <rustameynaliyev@Rustams-MacBook-Pro.local>
2018-12-27 18:39:07 +0400

    add: preloading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
+import { Routes, RouterModule, PreloadAllModules } from "@angular/router";
 
 const routes: Routes = [
   { path: "", redirectTo: "home", pathMatch: "full" },
-  { path: "home", loadChildren: "./home/home.module#HomePageModule" }
+  { path: "home", loadChildren: "./home/home.module#HomePageModule" },
+  {
+    path: "chatroom",
+    loadChildren: "./chatroom/chatroom.module#ChatroomPageModule"
+  }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      enableTracing: true, // <-- debugging purposes only
+      preloadingStrategy: PreloadAllModules
+    })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
