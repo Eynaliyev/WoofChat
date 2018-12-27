5bd0eb1c0afd1226a76d7c5a8431d0eca512071f
Rustam Eynaliyev <rustameynaliyev@Rustams-MacBook-Pro.local>
2018-12-27 18:59:50 +0400

    update: tabs instead of spaces

--- src/app/chatroom/chatroom.page.ts
+++ src/app/chatroom/chatroom.page.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-chatroom',
-  templateUrl: './chatroom.page.html',
-  styleUrls: ['./chatroom.page.scss'],
+	selector: "app-chatroom",
+	templateUrl: "./chatroom.page.html",
+	styleUrls: ["./chatroom.page.scss"]
 })
 export class ChatroomPage implements OnInit {
+	constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+	ngOnInit() {}
 }
