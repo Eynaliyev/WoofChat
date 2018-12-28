1161f1930a986333d0d6fed1e5999b133e814211
Rustam Eynaliyev <rustameynaliyev@Rustams-MacBook-Pro.local>
2018-12-28 23:13:57 +0400

    remove: chatroom: add: gmap config info

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,19 @@ import { Routes, RouterModule, PreloadAllModules } from "@angular/router";
 const routes: Routes = [
 	{ path: "", redirectTo: "home", pathMatch: "full" },
 	{ path: "home", loadChildren: "./home/home.module#HomePageModule" },
-	{
-		path: "chatroom",
-		loadChildren: "./chatroom/chatroom.module#ChatroomPageModule"
-	},
 	{ path: "login", loadChildren: "./login/login.module#LoginPageModule" },
-  { path: 'contacts', loadChildren: './contacts/contacts.module#ContactsPageModule' },
-  { path: 'user-profile', loadChildren: './user-profile/user-profile.module#UserProfilePageModule' },
-  { path: 'my-profile', loadChildren: './my-profile/my-profile.module#MyProfilePageModule' }
+	{
+		path: "contacts",
+		loadChildren: "./contacts/contacts.module#ContactsPageModule"
+	},
+	{
+		path: "user-profile",
+		loadChildren: "./user-profile/user-profile.module#UserProfilePageModule"
+	},
+	{
+		path: "my-profile",
+		loadChildren: "./my-profile/my-profile.module#MyProfilePageModule"
+	}
 ];
 
 @NgModule({
