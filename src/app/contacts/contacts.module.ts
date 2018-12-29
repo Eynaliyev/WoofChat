3f838a030d68ef90531f69c4f8ddfeffb972d803
Rustam Eynaliyev <rustameynaliyev@Rustams-MacBook-Pro.local>
2018-12-29 16:27:01 +0400

    fix: indentation

--- src/app/contacts/contacts.module.ts
+++ src/app/contacts/contacts.module.ts
@@ -1,26 +1,26 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { Routes, RouterModule } from "@angular/router";
 
-import { IonicModule } from '@ionic/angular';
+import { IonicModule } from "@ionic/angular";
 
-import { ContactsPage } from './contacts.page';
+import { ContactsPage } from "./contacts.page";
 
 const routes: Routes = [
-  {
-    path: '',
-    component: ContactsPage
-  }
+	{
+		path: "",
+		component: ContactsPage
+	}
 ];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RouterModule.forChild(routes)
-  ],
-  declarations: [ContactsPage]
+	imports: [
+		CommonModule,
+		FormsModule,
+		IonicModule,
+		RouterModule.forChild(routes)
+	],
+	declarations: [ContactsPage]
 })
 export class ContactsPageModule {}
