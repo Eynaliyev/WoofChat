00750b8c5ca8bdc82c863922fd91d0c645af1170
Rustam Eynaliyev <rustameynaliyev@Rustams-MacBook-Pro.local>
2018-12-29 16:28:02 +0400

    fix: styles in specs

--- src/app/my-profile/my-profile.page.spec.ts
+++ src/app/my-profile/my-profile.page.spec.ts
@@ -1,27 +1,26 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { MyProfilePage } from './my-profile.page';
+import { MyProfilePage } from "./my-profile.page";
 
-describe('MyProfilePage', () => {
-  let component: MyProfilePage;
-  let fixture: ComponentFixture<MyProfilePage>;
+describe("MyProfilePage", () => {
+	let component: MyProfilePage;
+	let fixture: ComponentFixture<MyProfilePage>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ MyProfilePage ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    })
-    .compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [MyProfilePage],
+			schemas: [CUSTOM_ELEMENTS_SCHEMA]
+		}).compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MyProfilePage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(MyProfilePage);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it("should create", () => {
+		expect(component).toBeTruthy();
+	});
 });
